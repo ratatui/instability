d8798428710c24301c2a35da105457abb49ba623
Josh McKinney <joshka@users.noreply.github.com>
2024-05-01 00:28:27 -0700

    docs: tweak doc wording and formatting

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,22 @@
-//! This crate provides attribute macros for specifying API stability of public
-//! API items of a crate.
+//! This crate provides attribute macros for specifying API stability of public API items of a
+//! crate.
 //!
-//! The Rust standard library has a concept of [API
-//! stability](https://rustc-dev-guide.rust-lang.org/stability.html) and custom
-//! attributes for managing that on a per-item basis, but most of these
-//! attributes are not available for normal crates to use, with the exception of
-//! the
-//! [`#[deprecated]`](https://doc.rust-lang.org/reference/attributes/diagnostics.html#the-deprecated-attribute)
-//! attribute. This crate seeks to provide similar attributes on stable Rust,
-//! though tuned more toward what the needs of normal crate authors.
+//! The Rust standard library has a concept of [API stability] and custom attributes for managing
+//! that on a per-item basis, but most of these attributes are not available for normal crates to
+//! use, with the exception of the [`deprecated`] attribute. This crate seeks to provide similar
+//! attributes on stable Rust, though tuned more toward what the needs of normal crate authors.
 //!
-//! For complete examples of how to use this crate, check out the source code
-//! for the [`instability-example`
-//! crate](https://github.com/ratatui-org/instability/tree/master/example) included
-//! in the instability repository.
+//! For complete examples of how to use this crate, check out the source code for the
+//! [`instability-example`] crate in the repository
 //!
-//! Currently, only the [`#[unstable]`][macro@unstable] attribute is available.
-//! Please see the documentation of that macro for an explanation on what it
-//! does and how to use it.
+//! Currently, only the [`unstable`] attribute is available. Please see the documentation of that
+//! macro for an explanation on what it does and how to use it.
+//!
+//! [API stability]: https://rustc-dev-guide.rust-lang.org/stability.html
+//! [`deprecated`]:
+//!     https://doc.rust-lang.org/reference/attributes/diagnostics.html#the-deprecated-attribute
+//! [`instability-example`]: https://github.com/ratatui-org/instability/tree/master/example
+//! [`unstable`]: macro@unstable
 
 use proc_macro::TokenStream;
 use syn::{parse_macro_input, Item};
@@ -26,47 +25,42 @@ mod unstable;
 
 /// Mark an API as unstable.
 ///
-/// You can apply this attribute to an item in your public API that you would
-/// like to expose to users, but are not yet ready for general use. This is
-/// useful when you want to let users try out some new functionality for an API
-/// you haven't finished testing or designing, or for whatever reason do not
-/// want to commit any stability guarantees for.
+/// You can apply this attribute to an item in your public API that you would like to expose to
+/// users, but are not yet ready for general use. This is useful when you want to let users try out
+/// some new functionality for an API you haven't finished testing or designing, or for whatever
+/// reason do not want to commit any stability guarantees for.
 ///
 /// This attribute does the following things to annotated items:
 ///
-/// - Changes the visibility of the item from `pub` to `pub(crate)`, unless a
-///   certain crate feature is enabled. This ensures that internal code within
-///   the crate can always use the item, but downstream consumers cannot access
-///   it unless they opt-in to the unstable API.
-///   - Visibility of certain child items of the annotated item will also be
-///     changed to match the new item visibility, such as struct fields. Children
-///     that are not public will not be affected.
-///   - Child items of annotated modules will *not* have their visibility changed,
-///     as it might be desirable to be able to re-export them even if the module
-///     visibility is restricted. You should apply the attribute to each item
-///     within the module with the same feature name if you want to restrict the
-///     module's contents itself and not just the module namespace.
-/// - Appends an "Availability" section to the item's documentation that notes
-///   that the item is unstable, and indicates the name of the crate feature to
-///   enable it.
+/// - Changes the visibility of the item from `pub` to `pub(crate)` unless a certain crate feature
+///   is enabled. This ensures that internal code within the crate can always use the item, but
+///   downstream consumers cannot access it unless they opt-in to the unstable API.
+/// - Changes the Visibility of certain child items of the annotated item (such as struct fields) to
+///   match the item's visibility. Children that are not public will not be affected.
+/// - Appends an "Availability" section to the item's documentation that notes that the item is
+///   unstable and indicates the name of the crate feature to enable it.
 ///
-/// Note that unlike the `#[unstable]` attribute used [in the standard
-/// library](https://rustc-dev-guide.rust-lang.org/stability.html), this
-/// attribute does not apply itself recursively to child items.
+/// Child items of annotated modules are unchanged, as it might be desirable to be able to re-export
+/// them even if the module visibility is restricted. You should apply the attribute to each item
+/// within the module with the same feature name if you want to restrict the module's contents
+/// itself and not just the module namespace.
+///
+/// Note that unlike the [`unstable`][std-unstable] attribute used in the standard library, this attribute does
+/// not apply itself recursively to child items.
+///
+/// [std-unstable]: https://rustc-dev-guide.rust-lang.org/stability.html
 ///
 /// Applying this attribute to non-`pub` items is pointless and does nothing.
 ///
 /// # Arguments
 ///
-/// The `unstable` attribute supports optional arguments that can be passed to
-/// control its behavior.
+/// The `unstable` attribute supports optional arguments that can be passed to control its behavior.
 ///
-/// - `feature`: Specify the name of the unstable feature that should control
-///   this item's availability. The crate feature will have the string
-///   `unstable-` prepended to it. If not specified, it will be guarded by a
-///   catch-all `unstable` feature.
-/// - `issue`: Provide a link or reference to a tracking issue for the unstable
-///   feature. This will be included in the item's documentation.
+/// - `feature`: the name of the unstable feature that should control this item's availability. This
+///   will have the string `unstable-` prepended to it. If not specified, the item will instead be
+///   guarded by a catch-all `unstable` feature.
+/// - `issue`: a link or reference to a tracking issue for the unstable feature. This will be
+///   included in the item's documentation.
 ///
 /// # Examples
 ///
@@ -91,9 +85,9 @@ mod unstable;
 /// ///
 /// /// # Availability
 /// ///
-/// /// **This API is marked as unstable** and is only available when the
-/// /// `unstable-risky-function` crate feature is enabled. This comes with no
-/// /// stability guarantees, and could be changed or removed at any time.
+/// /// **This API is marked as unstable** and is only available when the `unstable-risky-function`
+/// /// crate feature is enabled. This comes with no stability guarantees, and could be changed or
+/// /// removed at any time.
 /// #[cfg(feature = "unstable-risky-function")]
 /// pub fn risky_function() {
 ///     unimplemented!()
