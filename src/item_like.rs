cf3e49aa1667fe619fa0d779609bee10f14ce71b
Dániel Buga <bugadani@gmail.com>
2025-01-10 17:11:40 +0100

    Add #[allow(unused_imports)] lint to unstable reexports (#21)
    Reexports marked as unstable no longer need to explicitly allow
    the unused_imports lint.
    
    ---------
    
    Co-authored-by: Josh McKinney <joshka@users.noreply.github.com>

--- src/item_like.rs
+++ src/item_like.rs
@@ -15,11 +15,33 @@ pub trait ItemLike: Stability {
     fn is_public(&self) -> bool {
         matches!(self.visibility(), Visibility::Public(_))
     }
+
+    fn allowed_lints(&self) -> Vec<syn::Ident>;
 }
 
-macro_rules! impl_has_visibility {
-($($ty:ty),+ $(,)?) => {
-    $(
+/// Implement `ItemLike` for the given type.
+///
+/// This makes each of the syn::Item* types implement our `ItemLike` trait to make it possible to
+/// work with them in a more uniform way.
+///
+/// A single type can be passed to this macro, or multiple types can be passed at once.
+/// Each type can be passed with a list of lints that are allowed for that type (defaulting to
+/// `dead_code` if not specified).
+macro_rules! impl_item_like {
+    // run impl_item_like for each item in a list of items
+    ($($(#[allow($($lint:ident),*)])? $ty:ty ),+ ,) => {
+        $(
+            impl_item_like!($(#[allow($($lint),*)])? $ty );
+        )*
+    };
+
+    // run impl_item_like for a single item without any lints
+    ($ty:ty) => {
+        impl_item_like!(#[allow(dead_code)] $ty );
+    };
+
+    // Implement `ItemLike` for the given type.
+    (#[allow($($lint:ident),*)] $ty:ty) => {
         impl Stability for $ty {
             fn attrs(&self) -> &[syn::Attribute] {
                 &self.attrs
@@ -38,12 +60,18 @@ macro_rules! impl_has_visibility {
             fn set_visibility(&mut self, visibility: Visibility) {
                 self.vis = visibility;
             }
+
+            fn allowed_lints(&self) -> Vec<syn::Ident> {
+                vec![
+                    $(syn::Ident::new(stringify!($lint), proc_macro2::Span::call_site()),)*
+                ]
+            }
         }
-    )*
-};
+    };
+
 }
 
-impl_has_visibility!(
+impl_item_like!(
     syn::ItemType,
     syn::ItemEnum,
     syn::ItemFn,
@@ -51,6 +79,7 @@ impl_has_visibility!(
     syn::ItemTrait,
     syn::ItemConst,
     syn::ItemStatic,
+    #[allow(unused_imports)]
     syn::ItemUse,
 );
 
@@ -78,6 +107,10 @@ impl ItemLike for syn::ItemStruct {
             .for_each(|field| field.vis = visibility.clone());
 
         self.vis = visibility;
+    }
+
+    fn allowed_lints(&self) -> Vec<syn::Ident> {
+        vec![syn::Ident::new("dead_code", proc_macro2::Span::call_site())]
     }
 }
 
